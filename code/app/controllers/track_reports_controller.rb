d0523bb5f3f655ec470f476de594ae28b76c3493
Brian Rentsch <rentscb1@tcnj.edu>
2019-04-04 23:56:53 -0400

    Added 'track a report' navigation

---
+++ code/app/controllers/track_reports_controller.rb
@@ -0,0 +1,8 @@
+##
+# An class representing a controller which handles HTTP requests relating to the track_reports page.
+class TrackReportsController < ApplicationController
+  ##
+  # Get /track_reports/index
+  def index
+  end
+end
