27d517435af3a5b22b02b9aefcd4263bf1b118b9
Brian Rentsch <rentscb1@tcnj.edu>
2019-04-04 23:11:33 -0400

    Added homepage

---
+++ code/app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+##
+# An class representing a controller which handles HTTP requests relating to the home page.
+class HomeController < ApplicationController
+  ##
+  # Get /home/index
+  def index
+  end
+end
