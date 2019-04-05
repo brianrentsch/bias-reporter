1a183272e6b9f14cbaf0fb67de6c2c06251b9aa6
Brian Rentsch <rentscb1@tcnj.edu>
2019-04-04 22:20:57 -0400

    Converted database from SQLite to postgreSQL

--- code/db/schema.rb
+++ code/db/schema.rb
@@ -12,19 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_03_04_223804) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reports", force: :cascade do |t|
     t.string "date"
     t.string "time"
     t.string "location"
     t.text "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "school"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
