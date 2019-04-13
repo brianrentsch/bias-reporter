d25901c3c3a490e9a772cdc4cad275846933b6ad
Brian Rentsch <rentscb1@tcnj.edu>
2019-04-13 11:26:16 -0400

    Added about page controller generation

---
+++ code/app/controllers/about_controller.rb
@@ -0,0 +1,9 @@
+##
+# A class representing a controller which handles HTTP requests relating to the about page.
+class AboutController < ApplicationController
+  ##
+  # GET /about/index
+  def index
+  end
+end
+
