68ee0696767b93cef363b8683f6976763f8fe9c5
Brian Rentsch <rentscb1@tcnj.edu>
2019-04-13 10:41:05 -0400

    Added test files for new pages

---
+++ code/test/controllers/track_reports_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class TrackReportsControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get index" do
+    get track_reports_index_url
+    assert_response :success
+  end
+end
