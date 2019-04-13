7f487184f4ef95e7fe623b7a2819fb96f7c90c2b
Brian Rentsch <rentscb1@tcnj.edu>
2019-04-13 10:40:07 -0400

    Removed functionality from 'login' link, as will be added later

--- code/app/controllers/track_reports_controller.rb
+++ code/app/controllers/track_reports_controller.rb
@@ -2,7 +2,14 @@
 # An class representing a controller which handles HTTP requests relating to the track_reports page.
 class TrackReportsController < ApplicationController
   ##
-  # Get /track_reports/index
+  # GET /track_reports/index
   def index
   end
+ 
+  ##
+  # GET /track_reports/1
+  def show
+  end
+
+
 end
